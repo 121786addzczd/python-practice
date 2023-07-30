dcce3cc32e33e5e316b342cc78c99c6111944ea8
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2023-07-30 22:46:36 +0900

    feat:UserProfileクラスを作成してnameとbirthdayを同じ入れ物のデータへ格納

--- src/cli/fortune/fortune.py
+++ src/cli/fortune/fortune.py
@@ -13,35 +13,48 @@ LUCKY_COLORS = ["red", "green", "blue"]
 LUCKY_NUMBERS = [1, 2, 3]
 
 
-def tell_fortune(name: str, birthday: date, today: date) -> str:
+class UserProfile:
+    def __init__(self, name: str, birthday: date) -> None:
+        """
+        UserProfile クラスの初期化メソッド。
+
+        Args:
+            name (str): ユーザの名前
+            birthday (date): ユーザの誕生日
+        """
+        self.name = name
+        self.birthday = birthday
+
+
+def tell_fortune(user_profile: UserProfile, today: date) -> str:
     """
-    与えられた名前、誕生日、今日の日付に基づいて、運勢の文字列を返す
+    与えられた UserProfile オブジェクトと今日の日付に基づいて、運勢の文字列を返す。
 
     Args:
-        name (str): 運勢を占う対象の人物の名前
-        birthday (date): その人物の誕生日
-        today (date): 今日の日付
+        user_profile (UserProfile): 運勢を占う対象の UserProfile オブジェクト。
+        today (date): 今日の日付。
 
     Returns:
-        str: その人物の運勢を含む文字列
+        str: その人物の運勢を含む文字列。
     """
     lucky_color = random.choice(LUCKY_COLORS)
     lucky_number = random.choice(LUCKY_NUMBERS)
 
     return FORTUNE_OUTPUT_TEMPLATE.format(
         today=today,
-        name=name,
+        name=user_profile.name,
         lucky_color=lucky_color,
         lucky_number=lucky_number,
     )
 
 
-def main():
+def main() -> None:
     name = "ファルコ"
     birthday = date(2005, 4, 4)
+    user_profile = UserProfile(name, birthday)
     today = date.today()
 
-    result = tell_fortune(name, birthday, today)
+    result = tell_fortune(user_profile, today)
     print(result)
 
 
