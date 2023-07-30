481581ea6c843787d971e3a6c7d4cf88f01edef8
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2023-07-30 22:59:24 +0900

    feat:占いの処理をクラス化

--- src/cli/fortune/fortune.py
+++ src/cli/fortune/fortune.py
@@ -1,6 +1,6 @@
 import random
 from datetime import date
-from typing import Tuple
+from typing import List
 
 FORTUNE_OUTPUT_TEMPLATE = """
 {today} の {name} さんの運勢
@@ -8,9 +8,6 @@ FORTUNE_OUTPUT_TEMPLATE = """
 ラッキーカラー: {lucky_color}
 ラッキーナンバー: {lucky_number}
 """
-
-LUCKY_COLORS = ["red", "green", "blue"]
-LUCKY_NUMBERS = [1, 2, 3]
 
 
 class UserProfile:
@@ -26,26 +23,38 @@ class UserProfile:
         self.birthday = birthday
 
 
-def tell_fortune(user_profile: UserProfile, today: date) -> str:
-    """
-    与えられた UserProfile オブジェクトと今日の日付に基づいて、運勢の文字列を返す。
+class RandomFortuneTeller:
+    def __init__(self, lucky_colors: List[str], lucky_numbers: List[int]) -> None:
+        """
+        RandomFortuneTeller クラスの初期化メソッド。
 
-    Args:
-        user_profile (UserProfile): 運勢を占う対象の UserProfile オブジェクト。
-        today (date): 今日の日付。
+        Args:
+            lucky_colors (List[str]): 選択肢となるラッキーカラーのリスト
+            lucky_numbers (List[int]): 選択肢となるラッキーナンバーのリスト
+        """
+        self.lucky_colors = lucky_colors
+        self.lucky_numbers = lucky_numbers
 
-    Returns:
-        str: その人物の運勢を含む文字列。
-    """
-    lucky_color = random.choice(LUCKY_COLORS)
-    lucky_number = random.choice(LUCKY_NUMBERS)
+    def tell(self, user_profile: UserProfile, today: date) -> str:
+        """
+        与えられた UserProfile オブジェクトと今日の日付に基づいて、運勢の文字列を返す。
 
-    return FORTUNE_OUTPUT_TEMPLATE.format(
-        today=today,
-        name=user_profile.name,
-        lucky_color=lucky_color,
-        lucky_number=lucky_number,
-    )
+        Args:
+            user_profile (UserProfile): 運勢を占う対象の UserProfile オブジェクト。
+            today (date): 今日の日付。
+
+        Returns:
+            str: その人物の運勢を含む文字列。
+        """
+        lucky_color = random.choice(self.lucky_colors)
+        lucky_number = random.choice(self.lucky_numbers)
+
+        return FORTUNE_OUTPUT_TEMPLATE.format(
+            today=today,
+            name=user_profile.name,
+            lucky_color=lucky_color,
+            lucky_number=lucky_number,
+        )
 
 
 def main() -> None:
@@ -54,7 +63,11 @@ def main() -> None:
     user_profile = UserProfile(name, birthday)
     today = date.today()
 
-    result = tell_fortune(user_profile, today)
+    LUCKY_COLORS = ["red", "green", "blue"]
+    LUCKY_NUMBERS = [1, 2, 3]
+    fortune_teller = RandomFortuneTeller(LUCKY_COLORS, LUCKY_NUMBERS)
+
+    result = fortune_teller.tell(user_profile, today)
     print(result)
 
 
