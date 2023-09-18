8996667a2726b1597ee8bd4b339416971a228a2c
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2023-09-18 10:01:08 +0900

    feat pytestコマンドお試し用にファイル作成

---
+++ src/pytest_Introduction/test_section_1.py
@@ -0,0 +1,9 @@
+# とりあえずpytest実行
+# 実行コマンドは「pytest test_section_1.py」
+# bash: pytest: command not foundと出力された場合は
+#「python -m pytest test_section_1.py」を試してください
+# python -m pytest test_section_1.py -sコマンド実行するとprint文表示されます
+
+
+def test_hello_world() -> None:
+    print("Hello World! ")
