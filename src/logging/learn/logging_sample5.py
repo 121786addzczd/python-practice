029a70e322aebb3f307fd54262b3dcd5edff12a4
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2023-10-01 16:53:09 +0900

    feat ロガーのローテーションについて学習

---
+++ src/logging/learn/logging_sample5.py
@@ -0,0 +1,43 @@
+# RotationFileHandler, TimeRotationFileHandler
+
+import logging
+import logging.handlers
+
+logger = logging.getLogger(__name__)
+
+r_handler = logging.handlers.RotatingFileHandler(
+    'logs/rotation_file.log',
+    maxBytes=1000, # 1000バイトでログをローテートする
+    backupCount=5,
+    encoding='utf-8'
+)
+
+t_handler = logging.handlers.TimedRotatingFileHandler(
+    'logs/rotating_time_file.log',
+    when='S', # M, D, W0-W6(曜日)
+    interval=10,
+    backupCount=5,
+    encoding='utf-8'
+)
+
+logger.setLevel(logging.DEBUG)
+
+r_handler.setLevel(logging.INFO)
+t_handler.setLevel(logging.INFO)
+
+sample_formatter = logging.Formatter('%(name)s-%(asctime)s-%(levelname)s-%(message)s',
+                                    datefmt='%Y-%m-%d %H:%M:%S')
+r_handler.setFormatter(sample_formatter)
+t_handler.setFormatter(sample_formatter)
+logger.addHandler(r_handler)
+logger.addHandler(t_handler)
+
+import time
+
+for _ in range(1000):
+    logger.debug('デバッグログ')
+    logger.info('インフォログ')
+    logger.error('エラーログ')
+    logger.warning('ワーニングログ')
+    logger.critical('クリティカルログ')
+    time.sleep(1)
