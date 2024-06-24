51f5f4e133da6eb407d75bfa42bd42c6b254fbc9
nataneno <72346611+121786addzczd@users.noreply.github.com>
2024-06-24 12:14:56 +0900

    Streamlitでのマジックコマンド使用

--- src/streamlit/python-streamlit-data-viz/streamlit_basics.py
+++ src/streamlit/python-streamlit-data-viz/streamlit_basics.py
@@ -1,22 +1,30 @@
 import streamlit as st
 import pandas as pd
 
-st.title('タイトル表示')
-st.header('ヘッダーの表示')
-st.subheader('サブヘッダーの表示')
-st.text('テキストの表示')
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [40, 30, 20, 10]
+})
+
+df
+
+x = 100
+x
+
+
+"""
+# マジックコマンドを使用
+文字列の表示
+
+```python
+import streamlit as st
+import pandas as pd
 
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [40, 30, 20, 10]
 })
 
-st.write(df)
-
-# 縦と横の長さ指定ができる
-st.dataframe(df, width=400, height=400)
-# 各列の最大値をハイライトさせる
-st.dataframe(df.style.highlight_max(axis=0))
-
-# 動きが固定されたテーブルになる
-st.table(df)
+print("Hello Python!")
+```
+"""
