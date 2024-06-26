c2f264e38d6914497e99ada452d8b013c6b42b78
nataneno <72346611+121786addzczd@users.noreply.github.com>
2024-06-26 17:28:22 +0900

    feat：ログイン画面のデザイン作成

--- src/FastAPI/fastapi-react-learning/react-learning/login-screen/src/components/pages/Login.js
+++ src/FastAPI/fastapi-react-learning/react-learning/login-screen/src/components/pages/Login.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Container, Box, Typography, TextField, Button } from '@mui/material';
+import { Link } from "react-router-dom"; 
 
 const Login = () => {
   return (
-    <div>ログイン画面</div>
+    <>
+      <Container maxWidth="xs">
+        <Box sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center", 
+        }}>
+          <Typography variant="h5">ログイン画面</Typography>
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="username"
+            label="名前"
+            id="username"
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="パスワード"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} >
+            ログイン
+          </Button>
+
+          <Link to="/register">新規登録はこちら</Link>
+        </Box>
+      </Container>
+    </>
   );
 }
 
