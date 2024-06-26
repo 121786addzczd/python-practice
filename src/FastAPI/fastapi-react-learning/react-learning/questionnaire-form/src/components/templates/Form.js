27c2a67d02980684b86b4ddae0b403c03cc3bf61
nataneno <72346611+121786addzczd@users.noreply.github.com>
2024-06-26 09:43:34 +0900

    アンケートフォームのスタイリング

--- src/FastAPI/fastapi-react-learning/react-learning/questionnaire-form/src/components/templates/Form.js
+++ src/FastAPI/fastapi-react-learning/react-learning/questionnaire-form/src/components/templates/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { Container, Box, Typography, TextField, InputLabel, Select, MenuItem, FormLabel, RadioGroup, FormControlLabel, Radio, Button } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 
 const Form = () => {
   const [form, setForm] = useState({
@@ -25,34 +26,55 @@ const Form = () => {
   };
 
   return (
-    <>
-      <form>
-        <label htmlFor="name">名前
-          <input id="name" type="text" name="name" value={form.name} onChange={handleInputChange} />
-        </label>
-        <br />
-        <label htmlFor="age">年齢
-          <select id="age" name="age" value={form.age} onChange={handleInputChange} >
-            <option value={10}>10代</option>
-            <option value={20}>20代</option>
-            <option value={30}>30代</option>
-            <option value={40}>40代</option>
-            <option value={50}>50代</option>
-          </select>
-        </label>
-        <br />
-        <label htmlFor="gender">性別
-          <input id="male" name="gender" type="radio" value="male" onChange={handleInputChange} />男性
-          <input id="female" name="gender" type="radio" value="female" onChange={handleInputChange} />女性
-          <input id="other" name="gender" type="radio" value="other" onChange={handleInputChange} />その他
-        </label>
-        <br />
-        <label htmlFor="comment">コメント
-          <textarea id="comment" name="comment" placeholder='コメントを入れてください' value={form.comment} onChange={handleInputChange}></textarea>
-        </label>
-      </form>
-      <button onClick={handleSubmit}>送信</button>
-    </>
+    <Container maxWidth="sm">
+      <Box sx={{
+        marginTop: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center", 
+      }}>
+        <Typography variant="h5">アンケート提出</Typography>
+        <TextField id="name" name="name" label="名前" variant="outlined" value={form.name} onChange={handleInputChange} fullWidth />
+        
+        <Box sx={{ width: "100%", mt: 2 }}>
+          <InputLabel id="demo-simple-select-label">年齢</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={form.age}
+            name="age"
+            onChange={handleInputChange}
+            fullWidth
+          >
+            <MenuItem value={10}>10代</MenuItem>
+            <MenuItem value={20}>20代</MenuItem>
+            <MenuItem value={30}>30代</MenuItem>
+            <MenuItem value={40}>40代</MenuItem>
+            <MenuItem value={50}>50代</MenuItem>
+          </Select>
+        </Box>
+
+        <Box sx={{ width: "100%", mt: 2 }}>
+          <FormLabel id="demo-radio-buttons-group-label">性別</FormLabel>
+          <Box sx={{ border: 1, borderRadius: 1, borderColor: "grey.500" }}>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="gender"
+            onChange={handleInputChange}
+          >
+            <FormControlLabel value="female" control={<Radio />} label="女性" />
+            <FormControlLabel value="male" control={<Radio />} label="男性" />
+            <FormControlLabel value="other" control={<Radio />} label="その他" />
+          </RadioGroup>
+          </Box>
+        </Box>
+
+        <TextField id="comment" name="comment" label="コメント" variant="outlined" value={form.comment} onChange={handleInputChange} fullWidth sx={{ mt: 2 }} />
+
+        <Button variant="contained" onClick={handleSubmit} sx={{ mt: 2 }} endIcon={<SendIcon />} fullWidth>送信</Button>
+      </Box>
+    </Container>
   );
 };
 
