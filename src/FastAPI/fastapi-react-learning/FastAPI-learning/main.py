4c94d23ee03e29564ef02d86c0ab072dded41c96
nataneno <72346611+121786addzczd@users.noreply.github.com>
2024-06-27 11:10:53 +0900

    パスパラメータ、クエリパラメータ、リクエストボディについて学習

--- src/FastAPI/fastapi-react-learning/FastAPI-learning/main.py
+++ src/FastAPI/fastapi-react-learning/FastAPI-learning/main.py
@@ -1,4 +1,14 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
@@ -8,6 +18,23 @@ def read_root():
     return {"Hello": "World"}
 
 
+# パスパラメータ
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id):
+    return {"item_id": item_id}
+#  http://127.0.0.1:8000/items/foo
+
+
+# クエリパラメータ
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+# http://127.0.0.1:8000/items/?skip=0&limit=10
+
+
+# リクエストボデイ
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
