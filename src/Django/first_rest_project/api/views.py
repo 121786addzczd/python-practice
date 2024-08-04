d55d6361b39b83a0a800175794b6a20b49724bbb
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-08-04 11:31:42 +0900

    エラーハンドリングの追加

--- src/Django/first_rest_project/api/views.py
+++ src/Django/first_rest_project/api/views.py
@@ -4,7 +4,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from datetime import datetime, timezone
 import pytz
-
+from pytz.exceptions import UnknownTimeZoneError
+from rest_framework import status
 
 def index(request):
     return HttpResponse('<h1>Hello World</h1>')
@@ -18,7 +19,10 @@ def country_datetime(request):
         # print(request.data)
         requested_timezone = request.data.get('timezone')
         if requested_timezone:
-            tz = pytz.timezone(requested_timezone)
+            try:
+                tz = pytz.timezone(requested_timezone)
+            except UnknownTimeZoneError as e:
+                return Response({"Error POST": "Timezone not exists"}, status=status.HTTP_400_BAD_REQUEST)
             utc_datetime = datetime.now(timezone.utc)
             return Response({f"DateTime POST: {requested_timezone}": utc_datetime.astimezone(tz)})
     elif request.method == 'PUT':
@@ -34,7 +38,10 @@ def country_datetime(request):
         """
         requested_timezone = request.query_params.get('timezone')
         if requested_timezone:
-            tz = pytz.timezone(requested_timezone)
+            try:
+                tz = pytz.timezone(requested_timezone)
+            except UnknownTimeZoneError as e:
+                return Response({"Error GET": "Timezone not exists"}, status=status.HTTP_400_BAD_REQUEST)
             utc_datetime = datetime.now(timezone.utc)
             return Response({f"DateTime GET: {requested_timezone}": utc_datetime.astimezone(tz)})
     return Response({"Datetime": datetime.now()})
