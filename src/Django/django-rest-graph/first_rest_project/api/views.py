8c0c7273046c4a0d91a4c8c4e4689dd5486bd182
121786addzczd <1217hs2000ap1@gmail.com>
2025-11-02 19:16:33 +0900

    GETリクエストとPOSTリクエストの実装

--- src/Django/django-rest-graph/first_rest_project/api/views.py
+++ src/Django/django-rest-graph/first_rest_project/api/views.py
@@ -2,13 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from datetime import datetime
+from datetime import datetime, timezone
+import pytz
 
 def index(request):
     # return HttpResponse("<h1>Hello, this is the API index page.</h1>") # HTML response
     return JsonResponse({"message": "Hello, json"}) # JSON response
 
 # 時間を返す
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def country_datetime(request):
+    print(f"RequestMethod={request.method}")
+    if request.method == 'POST':
+        print(f"RequestBody={request.data}")
+        requested_timezone = request.data.get('timezone')
+        if requested_timezone:
+            tz = pytz.timezone(requested_timezone)
+            utc_datetime = datetime.now(timezone.utc)
+            return Response(
+                {f"DateTime POST: {requested_timezone}": utc_datetime.astimezone(tz)}
+            )
+    else:
+        requested_timezone = request.query_params.get('timezone')
+        if requested_timezone:
+            tz = pytz.timezone(requested_timezone)
+            utc_datetime = datetime.now(timezone.utc)
+            return Response(
+                {f"DateTime GET: {requested_timezone}": utc_datetime.astimezone(tz)}
+            )
     return Response({"DateTime": datetime.now()})
