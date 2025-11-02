6cbda24fa3b7d276b23f3c8a1634d642d6970c87
121786addzczd <1217hs2000ap1@gmail.com>
2025-11-02 18:47:12 +0900

    DRFを用いてGETリクエストを返すapi作成

--- src/Django/django-rest-graph/first_rest_project/api/urls.py
+++ src/Django/django-rest-graph/first_rest_project/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'api'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('country_datetime/', views.country_datetime, name='country_datetime'),
 ]
--- src/Django/django-rest-graph/first_rest_project/api/views.py
+++ src/Django/django-rest-graph/first_rest_project/api/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from datetime import datetime
 
 def index(request):
     # return HttpResponse("<h1>Hello, this is the API index page.</h1>") # HTML response
     return JsonResponse({"message": "Hello, json"}) # JSON response
+
+# 時間を返す
+@api_view(['GET'])
+def country_datetime(request):
+    return Response({"DateTime": datetime.now()})
