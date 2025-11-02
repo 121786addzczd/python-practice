f48d0465f582234dc81b6b3078a1771af82bfcfa
121786addzczd <1217hs2000ap1@gmail.com>
2025-11-02 21:56:39 +0900

    serializerにバリデーション追加

--- src/Django/django-rest-graph/api_view_project/api/serializers.py
+++ src/Django/django-rest-graph/api_view_project/api/serializers.py
@@ -3,4 +3,26 @@ from rest_framework import serializers
 class ItemsSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=20)
     price = serializers.IntegerField(min_value=0)
+    discounted_price = serializers.IntegerField(min_value=0) # 割引き価格
+
+    def validate_name(self, value): # nameに対するバリデーション
+        print(f"name={value}")
+        if value[0].islower():
+            raise serializers.ValidationError('最初の文字は大文字にしてください')
+        return value 
+
+    def validate_price(self, value): # priceに対するバリデーション
+        print(f"price={value}")
+        # 1桁目が0以外を弾く(11, 101, 1108など)
+        if value % 10 !=0:
+            raise serializers.ValidationError('1桁目は0にしてください')
+        return value 
     
+    # 複数のデータにまたがっててのバリデーション
+    def validate(self, data):
+        print(f"data: {data}")
+        price = data.get('price')
+        discounted_price = data.get('discounted_price') 
+        if discounted_price>= price:
+            raise serializers.ValidationError('割引き価格は通常価格よりも低く設定してください')
+        return data
