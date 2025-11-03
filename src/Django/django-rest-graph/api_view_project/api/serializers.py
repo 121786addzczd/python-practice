118d7ddfaafcf84778716d7ed34697a3c666ea03
121786addzczd <1217hs2000ap1@gmail.com>
2025-11-03 10:14:37 +0900

    PATCH処理の追加

--- src/Django/django-rest-graph/api_view_project/api/serializers.py
+++ src/Django/django-rest-graph/api_view_project/api/serializers.py
@@ -12,12 +12,16 @@ class ItemsSerializer(serializers.Serializer):
     discounted_price = serializers.IntegerField(min_value=0, validators=[check_divide_by_ten,]) # 割引き価格
 
     def validate_name(self, value): # nameに対するバリデーション
+        if self.partial and value is None: # patchの場合はスキップ
+            return value
         print(f"name={value}")
         if value[0].islower():
             raise serializers.ValidationError('最初の文字は大文字にしてください')
         return value 
 
     def validate_price(self, value): # priceに対するバリデーション
+        if self.partial and value is None: # patchの場合はスキップ
+            return value
         print(f"price={value}")
         # 1桁目が0以外を弾く(11, 101, 1108など)
         if value % 10 !=0:
@@ -27,8 +31,8 @@ class ItemsSerializer(serializers.Serializer):
     # 複数のデータにまたがっててのバリデーション
     def validate(self, data):
         print(f"data: {data}")
-        price = data.get('price')
-        discounted_price = data.get('discounted_price') 
+        price = data.get('price', self.instance.price)
+        discounted_price = data.get('discounted_price', self.instance.discounted_price) 
         if discounted_price>= price:
             raise serializers.ValidationError('割引き価格は通常価格よりも低く設定してください')
         return data
--- src/Django/django-rest-graph/api_view_project/api/views.py
+++ src/Django/django-rest-graph/api_view_project/api/views.py
@@ -59,3 +59,11 @@ class ItemDetailView(APIView):
         item = Item.objects.get(pk=pk)
         item.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, pk):
+        item = Item.objects.get(pk=pk)
+        print(request.data)
+        serializer = self.serializer_class(item, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response(serializer.data)
